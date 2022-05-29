fe5453ae0057b6f56f7d8b8a411a83f18f57a2d2
gm-nayeem <gmnayeem25@gmail.com>
2022-05-30 00:29:13 +0600

    added some solution

--- 805A.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-  int l,r;
-  scanf("%d %d",&l,&r);
-  if(l==r) printf("%d",l);
-  else printf("2");
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Codeforces_Solutions
-I included my codeforces solutions
